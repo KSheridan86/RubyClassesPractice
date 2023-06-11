02e685e75679557660a925f8bd12ab07f2d0e7c7
Ken Sheridan <kensheridan86@gmail.com>
2023-06-11 10:43:46 +0100

    attr_accessor method practice

--- lab.rb
+++ lab.rb
@@ -1,23 +1,37 @@
+# First pass at creating a class in Ruby
+
+# class Person
+#     def initialize(aName, aAge)
+#         @name = aName
+#         @age = aAge
+#     end
+
+#     def name=( aName )
+#         @name = aName
+#     end
+
+#     def name
+#         return @name
+#     end
+
+#     def age=( age )
+#         @age = aAge
+#     end
+
+#     def age
+#         return @age
+#     end
+
+# end
+
+# 2nd pass at creating a class in Ruby, using the attr_accessor method
 class Person
+
+    attr_accessor :name, :age
+
     def initialize(aName, aAge)
         @name = aName
         @age = aAge
-    end
-
-    def name=( aName )
-        @name = aName
-    end
-
-    def name
-        return @name
-    end
-
-    def age=( age )
-        @age = aAge
-    end
-
-    def age
-        return @age
     end
 
 end
